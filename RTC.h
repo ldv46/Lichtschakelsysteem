7550e8282bd835ff378a36379e91c8cfefad3a59
Michael M <michael@leomarders.nl>
2014-04-07 21:58:13 +0200

    Onnodige functies verwijderd

--- RTC.h
+++ RTC.h
@@ -1,27 +1,3 @@
-void printDigits(int digits)
-{
-    // utility function for digital clock display: prints preceding colon and leading 0
-    Serial.print(':');
-    if(digits < 10)
-        Serial.print('0');
-    Serial.print(digits);
-}
-
-void digitalClockDisplay(void)
-{
-    // digital clock display of the time
-    Serial.print(hour());
-    printDigits(minute());
-    printDigits(second());
-    Serial.print(' ');
-    Serial.print(day());
-    Serial.print(' ');
-    Serial.print(month());
-    Serial.print(' ');
-    Serial.print(year()); 
-    Serial.println(); 
-}
-
 void RTCopstart(){
   setSyncProvider(RTC.get);   // the function to get the time from the RTC
   if(timeStatus() != timeSet){
