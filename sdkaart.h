8354e7d56afefcc561d534f3cd062fb1a36ce2c3
Michael M <michael@leomarders.nl>
2014-04-27 22:07:28 +0200

    Rode LED bij fout in SDkaart

--- sdkaart.h
+++ sdkaart.h
@@ -8,20 +8,21 @@ const int sdChipSelect = 4;            // SD card chipSelect
 void SDinit(){
  digitalWrite(10, HIGH); // but turn off the W5100 chip!
   // initialize the SD card
-  
   Serial << F("Setting up SD card...\n");
-
   if (!card.init(SPI_FULL_SPEED, 4)) {
     Serial << F("card failed\n");
     has_filesystem = false;
+    LED('R');
   }
   // initialize a FAT volume
   if (!volume.init(&card)) {
     Serial << F("vol.init failed!\n");
     has_filesystem = false;
+    LED('R');
   }
   if (!root.openRoot(&volume)) {
     Serial << F("openRoot failed");
     has_filesystem = false;
+    LED('R');
   }
 }
