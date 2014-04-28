a44c3ed772fa4e2e8bec8f1ce0ce93fdb377e92b
Michael M <michael@leomarders.nl>
2014-04-28 20:53:36 +0200

    Index handles versimpeld (duh)

--- netwerk.h
+++ netwerk.h
@@ -1,25 +1,23 @@
 boolean file_handler(TinyWebServer& web_server);
 boolean index_handler(TinyWebServer& web_server);
 
-
 void send_file_name(TinyWebServer& web_server, const char* filename) {
   if (!filename) {
     web_server.send_error_code(404);
     web_server << F("Could not parse URL");
   } else {
-    TinyWebServer::MimeType mime_type
-      = TinyWebServer::get_mime_type_from_filename(filename);
+    TinyWebServer::MimeType mime_type = TinyWebServer::get_mime_type_from_filename(filename);
     web_server.send_error_code(200);
     if (file.open(&root, filename, O_READ)) {
-	  web_server.send_content_type(mime_type);
-	  web_server.end_headers();
+      web_server.send_content_type(mime_type);
+      web_server.end_headers();
       Serial << F("Read file "); Serial.println(filename);
       web_server.send_file(file);
       file.close();
-    } else {
-	  web_server.send_content_type("text/plain");
-	  web_server.end_headers();
-	  Serial << F("Could not find file: "); Serial.println(filename);
+  } else {
+      web_server.send_content_type("text/plain");
+      web_server.end_headers();
+      Serial << F("Could not find file: "); Serial.println(filename);
       web_server << F("Could not find file: ") << filename << "\n";
     }
   }
@@ -27,26 +25,14 @@ void send_file_name(TinyWebServer& web_server, const char* filename) {
 
 boolean file_handler(TinyWebServer& web_server) {
   char* filename = TinyWebServer::get_file_from_path(web_server.get_path());
+  Serial << filename;
   send_file_name(web_server, filename);
   free(filename);
   return true;
 }
 
 boolean index_handler(TinyWebServer& web_server) {
-  if (file.open(&root, "index.html", O_READ)) {
-    web_server.send_error_code(200);
-    web_server.send_content_type("text/html");
-    web_server.end_headers();
-    Serial << F("Index inladen");
-    web_server.send_file(file);
-    file.close();
-  }else {
-      web_server.send_error_code(200);
-      web_server.send_content_type("text/plain");
-      web_server.end_headers();
-      Serial << F("Geen index.html gevonden!");
-      web_server << F("Geen index.html gevonden!") << "\n";
-  }
+  send_file_name(web_server, "INDEX.HTML");
   return true;
 }
 
